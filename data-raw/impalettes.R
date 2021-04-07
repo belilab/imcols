ba7beb317ee202baeea3add38fbaf4cfbb582183
JFGraef <65733546+JFGraef@users.noreply.github.com>
2021-04-07 13:47:06 +0200

    update_2021_04_07

--- data-raw/impalettes.R
+++ data-raw/impalettes.R
@@ -3,9 +3,13 @@
 impalettes <- list(
   main  = imcols::im_cols("Chevalier3", "Dutch1", "Halifax1", "Lake2", "Lake3", "Lumina2", "Mint1", "Pale1", "Pine1", "Pine3", "Rush2", "Silver3", "Silver4"),
 
-  discrete = imcols::im_cols("Chevalier3", "Dutch1", "Lake3", "Pine1", "Rush2", "Mint1"),
+  discrete1 = imcols::im_cols("Chevalier3", "Rush1", "Lake3", "Pine1", "Lumina2", "Silver4"),
 
-  heatmap1 = imcols::im_cols("Lake3", "white", "Chevalier1", "Rush3"),
+  discrete2 = imcols::im_cols("Chevalier3", "Rush1", "Lake3", "Pine1"),
+
+  discrete_two_colors= imcols::im_cols( "Rush1", "Lake3"),
+
+  heatmap1 = imcols::im_cols("Lake3", "Lake4","white", "Chevalier1", "Rush3"),
 
   heatmap2  = imcols::im_cols("Lake3", "Chevalier3", "Rush2"),
 
