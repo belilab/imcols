5c45e3c33cbbc287fe210a256f9f2b27dd9b9132
JFGraef <65733546+JFGraef@users.noreply.github.com>
2021-04-07 14:06:23 +0200

    V7

--- data-raw/impalettes.R
+++ data-raw/impalettes.R
@@ -1,20 +1,34 @@
+## code to prepare `impalettes` dataset goes here
+
 impalettes <- list(
   main  = imcols::im_cols("Chevalier3", "Dutch1", "Halifax1", "Lake2", "Lake3", "Lumina2", "Mint1", "Pale1", "Pine1", "Pine3", "Rush2", "Silver3", "Silver4"),
-  
-  discrete = imcols::im_cols("Chevalier3", "Dutch1", "Lake3", "Pine1", "Rush2", "Mint1"),
-  
+
+  discrete1 = imcols::im_cols("Chevalier3", "Rush1", "Lake3", "Pine1", "Lumina2", "Silver4"),
+
+  discrete2 = imcols::im_cols("Chevalier3", "Rush1", "Lake3", "Pine1"),
+
+  discrete_two_colors= imcols::im_cols( "Rush1", "Lake3"),
+
+  heatmap1 = imcols::im_cols("Lake3", "Lake4","white", "Chevalier1", "Rush3"),
   heatmap1 = imcols::im_cols("Lake3", "white", "Chevalier1", "Rush3"),
-  
+
   heatmap2  = imcols::im_cols("Lake3", "Chevalier3", "Rush2"),
 
-      heatmap4 = imcols::im_cols("Lake3", "white", "Rush3"),
-    
-    
-    heatmap_hot = imcols::im_cols("Chevalier3", "Rush2"),
-    heatmap_hot = imcols::im_cols("white", "Rush3"),
-    
-    heatmap_cool = imcols::im_cols("Lake3", "Pine3"),
-    heatmap_cool = imcols::im_cols("Lake3", "white"))
+  heatmap3  = imcols::im_cols("Lake3", "Pale2", "Dutch1"),
+
+  heatmap4 = imcols::im_cols("Lake3", "white", "Rush3"),
+
+
+  heatmap_hot = imcols::im_cols("Chevalier3", "Rush2"),
+
+  heatmap_cool = imcols::im_cols("Lake3", "Pine3")
+
+)
+
+
+
 
 
 usethis::use_data(impalettes, overwrite = TRUE)
+
+
